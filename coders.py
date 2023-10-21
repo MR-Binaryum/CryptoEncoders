9227942248b2cf3d36ebf962b0d25b80ee79e7aa
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2023-10-21 02:13:44 +0000

    Update coders.py

--- coders.py
+++ coders.py
@@ -1,16 +1,17 @@
 import hashlib
-hashList=['md5 ','sha1 ','sha224 ', 'sha256 ', 'sha334 ', 'sha512 ' ]
+
+hashList=['md5 ','sha1 ','sha224 ', 'sha256 ', 'sha384 ', 'sha512 ' ]
 print("")
 print("Avalaible Hashes to encode")
 print("______________________________________________")
 print(''.join(hashList))
 print("----------------------------------------------")
 print("")
+
 word = input("Enter the password: ")
 algoritm = input("Enter the name of algoritm: ")
-if algoritm == "md5" or "sha1" or "sha224" or "sha256" or "sha384" or "sha512":
-    coders = hashlib.new(algoritm)
-if word == any:
-    coders.update.__str__(word)
-print()
-print("hash:",coders.hexdigest())
+
+convert = hashlib.new(algoritm)
+convert.update(word.encode()) 
+print("")
+print("hash:",convert.hexdigest())
